8dc815a49b86f6f7aa761ef74b6b8953e7d8b87c
TreeBoot <112956706+TreeBoot@users.noreply.github.com>
2023-04-15 23:36:16 +0200

    Added Anti-Mention

--- Events/Client/messageCreate.js
+++ Events/Client/messageCreate.js
@@ -8,5 +8,11 @@ module.exports = {
 
             message.channel.send({ content: `${message.author}, don't attempt to send links or moderator may have to punish you! :(`});
         }
+
+        if (message.content.includes("<@701488828946448464>") || message.content.includes("<@913764493211893760>") || message.content.includes("<@714774178372255745>")) {
+            
+
+            message.channel.send({ content: `${message.author}, don't attempt to ping one of the Directors or TreeBot or moderators may have to punish you! :(`});
+        }
     }
 }
